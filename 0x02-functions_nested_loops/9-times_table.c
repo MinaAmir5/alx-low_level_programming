d3e65e767d8fcb0e58d49c1478e814d649b1feb6
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-21 01:28:52 +0300

    0x02

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,14 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			if (k < 10 && j != 0)
+			else if (k < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else if (k >= 10 && j != 0)
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -31,6 +31,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
