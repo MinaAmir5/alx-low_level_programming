c6bc61a607e23e401b171fdd257160d922b5027d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-23 12:37:59 +0300

    0x02

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
 	printf("Infinite loop incoming :(\n");
 
@@ -15,7 +15,7 @@ int main(void)
 	/**
 	 * whhile (i < 10)
 	 * {
-	 *     putchar(i);
+	 *		putchar(i);
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
