09ccbf25879bf87624cf4e7f12ce83f15352b810
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-23 19:47:17 +0300

    0x04

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 char _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,7 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-
 
 #endif /* MAIN_H */
