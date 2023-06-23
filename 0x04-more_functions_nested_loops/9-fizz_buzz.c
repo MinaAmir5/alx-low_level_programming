0b248474bf63bbc4868d11fd1fa443c1c02d3e9d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-23 19:54:58 +0300

    0x04

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,17 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf(" Fizz");
-		} 
-		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz");
-		} 
-		else if (i % 3 == 0 && i % 5 == 0)
+			printf("Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
-		} 
-		else if (i == 1)
+			printf("FizzBuzz");
+		} else if (i == 1)
 		{
 			printf(" %d", i);
-		} 
-		else
+		} else
 		{
 			printf(" %d", i);
 		}
