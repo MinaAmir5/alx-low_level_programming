486a9f37e6308f16e3090af44b18a2368fbc4ffe
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 02:22:26 +0300

    0x06

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-/**
- * main - print ot magic numbers
- *
- * Return: 0
- */
-
 int main(void)
 {
 	int n;
 	int a[5];
-	int *p;
+	int* p;
 
 	a[2] = 1024;
 	p = &n;
-
 	/*
 	 * write your line of code here...
-	 * Remember
+	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
