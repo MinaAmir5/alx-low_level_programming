c8bb97d3dc262e17122aeffc89b352857605969d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 02:07:49 +0300

    0x06

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,33 +9,32 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n = -n;
 	}
 	if (n > 1000)
 	{
-		_putchar((n / 1000) + '0');
+		putchar((n / 1000) + '0');
 		n = n % 1000;
-		_putchar((n / 100) + '0');
+		putchar((n / 100) + '0');
 		n = n % 100;
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 	}
 	else if (n > 100)
 	{
-		_putchar((n / 100) + '0');
+		putchar((n / 100) + '0');
 		n = n % 100;
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 	}
 	else if (n > 10)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 	}
 	else
 	{
-		_putchar((n % 10) + '0');
-
+		putchar((n % 10) + '0');
 	}
 }
