d6f04f7e41a48d760f55643931fd3138028e351f
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 04:43:58 +0300

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -53,7 +53,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
+		temp_tot = (val1 + val2) + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 void print_number(int n);
 int _putchar(char c);
 void rev_string(char *n);
-char *infinite_add(char* n1, char* n2, char* r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
