d1bde245f4b6c66bd31c60b40438433b7e3c998e
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 04:52:37 +0300

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * rev_string- reverse array
  * @n : integer params
  * @n2: number two.
  * Return : 0
  */
+
 void rev_string(char *n)
 {
 	int i = 0;
@@ -13,6 +15,7 @@ void rev_string(char *n)
 
 	while (*(n + i) != '\0')
 		i++;
+
 	i--;
 
 	for (j = 0; j < i; j++, i--)
@@ -22,6 +25,7 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
+
 /**
  * infinite_add - adds two numbers
  * @n1: number one.
@@ -30,6 +34,7 @@ void rev_string(char *n)
  * @size_r: buffer size:
  * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
@@ -37,36 +42,50 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + i) != '\0')
 		i++;
+
 	while (*(n2 + j) != '\0')
 		j++;
+
 	i--;
 	j--;
+
 	if (j >= size_r || i >= size_r)
 		return (0);
+
 	while ((j >= 0 || i >= 0) || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
+
 		else
 			val1 = *(n1 + i) - '0';
+
 		if (j < 0)
 			val2 = 0;
+
 		else
 			val2 = *(n2 + j) - '0';
+
 		temp_tot = (val1 + val2) + overflow;
+
 		if (temp_tot >= 10)
 			overflow = 1;
+
 		else
 			overflow = 0;
+
 		if (digits >= (size_r - 1))
 			return (0);
+
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
 	}
+
 	if (digits == size_r)
 		return (0);
+
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
