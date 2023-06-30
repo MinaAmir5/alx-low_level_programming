9b1a35263b4c88be14cb4e6778e910f0d58b73c9
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 03:27:50 +0300

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -51,7 +51,7 @@ char* infinite_add(char* n1, char* n2, char* r, int size_r)
 		if (j < 0)
 			val2 = 0;
 		else
-			val2 = *(n1 + i) - '0';
+			val2 = *(n2 + j) - '0';
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
