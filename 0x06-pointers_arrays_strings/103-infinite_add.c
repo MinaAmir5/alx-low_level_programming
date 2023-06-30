f4f5d51fd4f46253fd49a5576a0dbeacdd27fa35
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 03:18:33 +0300

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,25 +9,64 @@
  * @size_r: buffer size:
  * Return: the pointer to dest.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+void rev_string(char* n)
 {
-	int i, rem = 0;
-	for (i = 0; i < size_r; i++)
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
 	{
-		rem = (n1[i] - '0') + (n2[i] - '0') + rem;
-		r[i] = (rem % 10) + '0';
-		if (rem > 9)
-		{
-			rem = 1;
-		}
-		else
-		{
-			rem = 0;
-		}
-		if (i == size_r - 1 && rem == 1)
-		{
-			return 0;
-		}
+		i++;
 	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+char* infinite_add(char* n1, char* n2, char* r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n1 + i) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n1 + i) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
