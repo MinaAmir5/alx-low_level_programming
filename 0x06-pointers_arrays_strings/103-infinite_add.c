d80a6a67b49e2e13bba0f08ccf9f0bcb9653fd6d
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 04:10:06 +0300

    0x06

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -35,7 +35,7 @@ void rev_string(char *n)
  * Return: the pointer to dest.
  */
 
-char* infinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
