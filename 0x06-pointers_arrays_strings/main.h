2e93fe6a03c6e18728082c38c8eff49dc0803ca4
Mina Amir <52263378+MinaAmir5@users.noreply.github.com>
2023-06-30 03:58:10 +0300

    0x06

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 void print_number(int n);
 void rev_string(char* n);
+char* infinite_add(char* n1, char* n2, char* r, int size_r);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
